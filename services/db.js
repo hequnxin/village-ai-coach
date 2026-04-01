f573f14985d82208c15896f279f2793d4f28e896
dcywgdw <2389393658@qq.com>
2026-04-01 18:51:47 +0800

    修复数据库目录不存在的问题

--- services/db.js
+++ services/db.js
@@ -1,7 +1,16 @@
 const Database = require('better-sqlite3');
 const path = require('path');
+const fs = require('fs');
 
+// 数据库文件路径
 const dbPath = path.join(__dirname, '../data/village.db');
+const dbDir = path.dirname(dbPath);
+
+// 确保目录存在
+if (!fs.existsSync(dbDir)) {
+  fs.mkdirSync(dbDir, { recursive: true });
+}
+
 const db = new Database(dbPath);
 db.pragma('foreign_keys = ON');
 
